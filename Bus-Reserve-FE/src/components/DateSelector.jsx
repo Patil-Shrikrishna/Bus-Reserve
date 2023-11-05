3018559cc6afbd2d8b5e468d3796ad6ba619b972
Patil-Shrikrishna <krishnapt1993@gmail.com>
2023-11-05 22:26:27 +0530

    [feat]: Fix DateSelector component[WIP]

---
+++ Bus-Reserve-FE/src/components/DateSelector.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import moment from "moment";
+import { BsChevronCompactLeft } from "react-icons/bs";
+import { BsChevronCompactRight } from "react-icons/bs";
+import Heading from "./Heading";
+
+const DateSelector = () => {
+  const [currentMonth, setCurrentMonth] = React.useState(moment().month());
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const currentYear = moment().year();
+  const daysInMonth = moment(
+    `${currentYear}-${currentMonth + 1}`,
+    "YYYY-MM"
+  ).daysInMonth();
+  const dates = [];
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    // const date = moment(`${year}-${currentMonth + 1}-${day}`, "YYYY-MM-DD");
+    dates.push(day);
+  }
+
+  const calender = dates.map((date, index) => (
+    <button
+      key={index}
+      value={moment(`${currentYear}-${currentMonth + 1}-${date}`, "YYYY-MM-DD")}
+      className=" flex flex-col items-center justify-center"
+    >
+      <Heading
+        heading={JSON.stringify(date)}
+        className="text-sm md:text-md xl:text-2xl font-bold"
+      />
+      <Heading
+        heading={moment(`${currentYear}-${currentMonth + 1}-${date}`).format(
+          "ddd"
+        )}
+        className="text-sm md:text-md xl:text-lg font-medium uppercase"
+      />
+    </button>
+  ));
+
+  const monthsArray = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClick = (item, index) => {
+    setCurrentMonth(index);
+    setIsOpen(!isOpen);
+  };
+  const list = monthsArray.map((item, index) => {
+    return (
+      <div
+        key={item}
+        onClick={() => handleClick(item, index)}
+        className="option px-4 py-2 hover:bg-gray-100 cursor-pointer "
+      >
+        {item}
+      </div>
+    );
+  });
+
+  React.useEffect(() => {
+    const calendar = document.getElementById("calendar");
+    const scrollLeft = document.getElementById("scrollLeft");
+    const scrollRight = document.getElementById("scrollRight");
+
+    if (calendar && scrollLeft && scrollRight) {
+      scrollLeft.addEventListener("click", function () {
+        calendar.scrollLeft -= 350; // Adjust the value to control the scroll distance
+      });
+
+      scrollRight.addEventListener("click", function () {
+        calendar.scrollLeft += 350; // Adjust the value to control the scroll distance
+      });
+    }
+  }, []);
+  return (
+    <div className="flex w-full items-center border border-custom-darkgray bg-custom-lightgray rounded-lg ">
+      <button className="" id="scrollLeft">
+        <BsChevronCompactLeft size={24} />
+      </button>
+      {/* <MonthSelector /> */}
+      <div className="relative inline-block text-left">
+        <div>
+          <button
+            type="button"
+            id="custom-select"
+            onClick={handleToggle}
+            className="-rotate-90 text-custom-green text-xl font-bold w-fit h-fit"
+          >
+            {monthsArray[currentMonth]}
+          </button>
+        </div>
+        <div
+          id="select-items"
+          className={`absolute z-10 mt-2 py-1 w-fit h-52 overflow-scroll no-scrollbar bg-white border border-gray-300 rounded-md shadow-md ${
+            isOpen ? "" : "hidden"
+          } `}
+        >
+          {list}
+        </div>
+      </div>
+      <div
+        id="calendar"
+        className="overflow-scroll no-scrollbar flex w-full items-start justify-start"
+      >
+        <div className="flex w-full items-center">
+          <div
+            id="calendar"
+            className="flex gap-4 md:gap-8 lg:gap-12 xl:gap-14 w-full"
+          >
+            {calender}
+          </div>
+        </div>
+      </div>
+      <button className="" id="scrollRight">
+        <BsChevronCompactRight size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default DateSelector;
--- Bus-Reserve-FE/src/pages/AvailableBus.jsx
+++ Bus-Reserve-FE/src/pages/AvailableBus.jsx
@@ -2,28 +2,9 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BusDetailCard from "../components/BusDetailCard";
-import DateComponent from "../components/DateComponent";
-import { BsChevronCompactLeft } from "react-icons/bs";
-import { BsChevronCompactRight } from "react-icons/bs";
-import MonthSelector from "../components/MonthSelector";
+import DateSelector from "../components/DateSelector";
 
 const AvailableBus = () => {
-  React.useEffect(() => {
-    const calendar = document.getElementById("calendar");
-    const scrollLeft = document.getElementById("scrollLeft");
-    const scrollRight = document.getElementById("scrollRight");
-
-    if (calendar && scrollLeft && scrollRight) {
-      scrollLeft.addEventListener("click", function () {
-        calendar.scrollLeft -= 350; // Adjust the value to control the scroll distance
-      });
-
-      scrollRight.addEventListener("click", function () {
-        calendar.scrollLeft += 350; // Adjust the value to control the scroll distance
-      });
-    }
-  }, []);
-
   return (
     <div>
       {/* Section 1: Navbar */}
@@ -37,21 +18,8 @@ const AvailableBus = () => {
         {/* Section 2.2 : Available bus details */}
         <div className="flex flex-col w-5/6 md:w-4/6 xxl:w-4/6 gap-4 items-center">
           {/* Section 2.2.1 : Date */}
-          <div className="flex w-full items-center border border-custom-darkgray bg-custom-lightgray rounded-lg ">
-            <button className="" id="scrollLeft">
-              <BsChevronCompactLeft size={24} />
-            </button>
-            <MonthSelector />
-            <div
-              id="calendar"
-              className="overflow-scroll no-scrollbar flex w-full items-start justify-start"
-            >
-              <DateComponent />
-            </div>
-            <button className="" id="scrollRight">
-              <BsChevronCompactRight size={24} />
-            </button>
-          </div>
+          <DateSelector />
+
           {/* Section 2.2.2 : Bus cards */}
           <div className="flex w-full flex-col overflow-scroll no-scrollbar h-screen gap-4">
             {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
