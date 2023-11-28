f365c9e1f76a862c810df7be05b818c467d2f729
Patil-Shrikrishna <krishnapt1993@gmail.com>
2023-11-28 21:36:42 +0530

    [feat]: Implemented redux persistent

--- Bus-Reserve-FE/src/redux/reducers/rootReducer.js
+++ Bus-Reserve-FE/src/redux/reducers/rootReducer.js
@@ -1,18 +1,12 @@
 import { combineReducers } from "redux";
 import updateJourneyReducer from "./journey/updateJourneyReducer";
 import tripsReducer from "./trips/tripsReducer";
-import updateBookingDataReducer from "./booking/updateBookingDataReducer";
+import updateBookingDataReducer from "./bookingData/updateBookingDataReducer";
+import bookingReducer from "./booking/bookingReducer";
 const rootReducer = combineReducers({
   updateJourney: updateJourneyReducer,
   trips: tripsReducer,
   updateBooking: updateBookingDataReducer,
+  booking: bookingReducer,
 });
 export default rootReducer;
-// export default (state, action) => {
-//   // Handle undefined state here to prevent the warning
-//   if (action.type === "@@redux/INIT") {
-//     return rootReducer(undefined, {});
-//   }
-
-//   return rootReducer(state, action);
-// };
--- Bus-Reserve-FE/src/store.js
+++ Bus-Reserve-FE/src/store.js
@@ -1,5 +1,15 @@
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers/rootReducer";
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
+
+// export default { store, persistor };
